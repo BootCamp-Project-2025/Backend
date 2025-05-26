4f579059467e0a0671782b184b5153e15b7910ba
DanielRiosLopez <Daniel.Rios@jala.university>
2025-05-26 18:31:03 -0400

    initial configuration

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 app.use(express.json());
@@ -6,5 +6,5 @@ app.use(express.json());
 // app.use('/equipos', equipoRouter);
 
 app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+    console.log("Server running on http://localhost:3000");
 });

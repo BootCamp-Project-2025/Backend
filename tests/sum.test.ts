bea3d99f3b8a940bf3f5948ff1bd22c2675fc16d
DanielRiosLopez <Daniel.Rios@jala.university>
2025-05-26 18:40:33 -0400

    initial configuration

--- tests/sum.test.ts
+++ tests/sum.test.ts
@@ -1,7 +1,7 @@
-import { sum } from '../src/contexts/ContextExample/domain/sum';
+import { sum } from "../src/contexts/ContextExample/domain/sum";
 
-describe('sum()', () => {
-    it('adds two numbers correctly', () => {
-        expect(sum(2, 3)).toBe(5);
-    });
+describe("sum()", () => {
+  it("adds two numbers correctly", () => {
+    expect(sum(2, 3)).toBe(5);
+  });
 });

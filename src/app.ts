a44b6a25e8785cf1f4eb12c0a370ed81233d9c01
cclaurevju <Carlos.Claure@jala.university>
2025-06-03 20:23:49 -0400

    chore(health-cors): add cors to project

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import healthRoutes from "./contexts/SystemHealth/presentation/http/routes/healthRoutes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/health", healthRoutes);

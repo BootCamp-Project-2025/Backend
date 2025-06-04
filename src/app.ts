ab7efad9060c1465adc72d70f70af74763b79d1e
cclaurevju <Carlos.Claure@jala.university>
2025-06-03 20:54:41 -0400

    feature(swagger): add swagger

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,16 @@ import express from "express";
 import cors from "cors";
 import healthRoutes from "./contexts/SystemHealth/presentation/http/routes/healthRoutes";
 
+import swaggerUi from "swagger-ui-express";
+import { swaggerDocs } from "./config/swagger";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/health", healthRoutes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 export default app;
--- src/contexts/SystemHealth/presentation/http/routes/healthRoutes.ts
+++ src/contexts/SystemHealth/presentation/http/routes/healthRoutes.ts
@@ -3,6 +3,18 @@ import healthController from "../controllers/healthController";
 
 const router = Router();
 
+/**
+ * @swagger
+ * /health:
+ *  get:
+ *      summary: Retrieves the API health status
+ *      responses:
+ *          200:
+ *              description: Everything is ok
+ *          500:
+ *              description: Everything is wrong
+ *
+ */
 router.get("", healthController.getHealthStatus);
 
 export default router;

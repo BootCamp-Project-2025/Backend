0f040b6ed382be11b83444860910bf74c3e0ef66
Elam Cano <elamcano16@gmail.com>
2025-06-05 18:06:42 -0300

    feature: add coverage

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,13 @@ import type { JestConfigWithTsJest } from "ts-jest";
 const config: JestConfigWithTsJest = {
   preset: "ts-jest",
   testEnvironment: "node",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!**/node_modules/**",
+    "!src/generated/**",
+    "!**/main.ts",
+  ],
 };
 
 export default config;

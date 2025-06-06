22f6da974dd6aa80ff206fd31957e2ecbc843d8d
jose <jernestocastro01@gmail.com>
2025-06-05 22:20:17 -0400

    fix: files deleted to change to lower case

--- src/contexts/LearningContext/domain/interfaces/ICourseService.ts
+++
@@ -1,7 +0,0 @@
-//erase rule once used
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-import { CourseDTO } from "../dtos/CourseDTO";
-
-export interface ICourseService {
-  getAllCourses(): Promise<CourseDTO[]>;
-}

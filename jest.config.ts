623515647ae81922d54604d51b83f81d7dceefde
Elam Cano <elamcano16@gmail.com>
2025-06-09 11:50:17 -0300

    fix: merge with dev

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,9 @@ import type { JestConfigWithTsJest } from "ts-jest";
 const config: JestConfigWithTsJest = {
   preset: "ts-jest",
   testEnvironment: "node",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
   collectCoverage: false,
   collectCoverageFrom: [
     "src/**/*.{ts,tsx}",

d312942d418896fa17511086405cb102ac5398db
JoseCaJala <Jose.Castro@jala.university>
2025-06-10 10:54:16 -0400

    fix: files were relocated to the application layer

--- src/d-containers.ts
+++ src/d-containers.ts
@@ -3,7 +3,7 @@ import { CourseRepository } from "./contexts/LearningContext/infraestructure/dat
 import { ICourseRepository } from "./contexts/LearningContext/domain/interfaces/ICourseRepository";
 import { GetAllCoursesUseCase } from "./contexts/LearningContext/application/useCases/GetAllCoursesUseCase";
 import { CreateCourseUseCase } from "./contexts/LearningContext/application/useCases/CreateCourseUseCase";
-import { CourseService } from "./contexts/LearningContext/infraestructure/services/CourseService";
+import { CourseService } from "./contexts/LearningContext/application/services/CourseService";
 import { ICourseService } from "./contexts/LearningContext/domain/interfaces/ICourseService";
 import { CourseController } from "./contexts/LearningContext/presentation/http/controllers/CourseController";
 import { IUseCase } from "@/contexts/LearningContext/domain/interfaces/IUseCase";

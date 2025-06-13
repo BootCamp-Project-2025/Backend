3ecd179620613a7067aeb20431d437edcbb18d68
Ariel Monroy <ariel.monroy@jala.university>
2025-06-13 00:09:57 -0400

    feat: enhance CertificationService with create, delete, update, and getById methods, and add CertificationDTO interface

--- src/contexts/CoreContext/application/services/CertificationService.ts
+++ src/contexts/CoreContext/application/services/CertificationService.ts
@@ -2,14 +2,51 @@ import { inject, injectable } from "tsyringe";
 import { Certification } from "../../domain/entities/Certification";
 import { ICertificationService } from "../../domain/interfaces/services/ICertificationService";
 import { GetCertificationsUseCase } from "../useCases/certifications/GetCertificationUseCase";
+import { CreateCertificationUseCase } from "../useCases/certifications/CreateCertificationUseCase";
+import { DeleteCertificationUseCase } from "../useCases/certifications/DeleteCertificationUseCase";
+import { UpdateCertificationUseCase } from "../useCases/certifications/UpdateCertificationUseCase";
+import { GetCertificationByIdUseCase } from "../useCases/certifications/GetCertificationByIdUseCase";
+import { CertificationDTO } from "../../domain/interfaces/dtos/ICertificationDto";
 
 @injectable()
 export class CertificationService implements ICertificationService {
   constructor(
     @inject("GetCertificationUseCase")
-    private getCertificationUseCase: GetCertificationsUseCase
+    private getCertificationUseCase: GetCertificationsUseCase,
+    @inject("CreateCertificationUseCase")
+    private createCertificationUseCase: CreateCertificationUseCase,
+    @inject("UpdateCertificationUseCase")
+    private updateCertificationUseCase: UpdateCertificationUseCase,
+    @inject("DeleteCertificationUseCase")
+    private deleteCertificationUseCase: DeleteCertificationUseCase,
+    @inject("GetCertificationById")
+    private getCertificationById: GetCertificationByIdUseCase
   ) {}
   getByFreelancerId(id: string): Promise<Certification[]> {
     return this.getCertificationUseCase.execute(id);
+  }
+  create(certification: CertificationDTO, freelancerId: string): Promise<void> {
+    return this.createCertificationUseCase.execute({
+      certification,
+      freelancerId,
+    });
+  }
+
+  delete(certificationId: string): Promise<void> {
+    return this.deleteCertificationUseCase.execute(certificationId);
+  }
+  update(
+    certificationId: string,
+    certification: Certification,
+    freelancerId: string
+  ): Promise<void> {
+    return this.updateCertificationUseCase.execute({
+      certificationId,
+      certification,
+      freelancerId,
+    });
+  }
+  getById(certificationId: string): Promise<Certification | null> {
+    return this.getCertificationById.execute(certificationId);
   }
 }
--- src/contexts/CoreContext/domain/interfaces/services/ICertificationService.ts
+++ src/contexts/CoreContext/domain/interfaces/services/ICertificationService.ts
@@ -1,5 +1,14 @@
 import { Certification } from "../../entities/Certification";
+import { CertificationDTO } from "../dtos/ICertificationDto";
 
 export interface ICertificationService {
   getByFreelancerId(id: string): Promise<Certification[]>;
+  create(certification: CertificationDTO, freelancerId: string): Promise<void>;
+  delete(certificationId: string): Promise<void>;
+  update(
+    certificationId: string,
+    certification: Certification,
+    freelancerId: string
+  ): Promise<void>;
+  getById(certificationId: string): Promise<Certification | null>;
 }

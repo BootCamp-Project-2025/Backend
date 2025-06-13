4f2590406f9f856fd08111bec83b7e7d42e6058a
Ariel Monroy <ariel.monroy@jala.university>
2025-06-13 00:09:33 -0400

    feat: update ICertificationRepository and CertificationRepository to include freelancerId in create and update methods

--- src/contexts/CoreContext/domain/interfaces/repositories/ICertificationRepository.ts
+++ src/contexts/CoreContext/domain/interfaces/repositories/ICertificationRepository.ts
@@ -2,8 +2,12 @@ import { Certification } from "../../entities/Certification";
 
 export interface ICertificationRepository {
   findByFreelancerId(freelancerId: string): Promise<Certification[]>;
-  create(certification: Certification): Promise<void>;
+  create(certification: Certification, freelancerId: string): Promise<void>;
   delete(certificationId: string): Promise<void>;
-  update(certificationId: string, certification: Certification): Promise<void>;
+  update(
+    certificationId: string,
+    certification: Certification,
+    freelancerId: string
+  ): Promise<void>;
   findById(certificationId: string): Promise<Certification | null>;
 }

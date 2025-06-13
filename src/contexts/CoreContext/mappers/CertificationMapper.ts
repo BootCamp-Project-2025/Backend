46c227b22777e141fdf6dfb4f30d801883cd8102
Ariel Monroy <ariel.monroy@jala.university>
2025-06-13 00:09:48 -0400

    feat: implement use cases for creating, deleting, and updating certifications with freelancerId support

--- src/contexts/CoreContext/mappers/CertificationMapper.ts
+++ src/contexts/CoreContext/mappers/CertificationMapper.ts
@@ -1,9 +1,12 @@
 import { UniqueEntityID } from "@/contexts/Shared/domain/UniqueEntityID";
 import { Certification as PrismaCertification } from "@/generated/prisma";
 import { Certification } from "../domain/entities/Certification";
+import { CertificationDTO } from "../domain/interfaces/dtos/ICertificationDto";
 
 export default class CertificationMapper {
-  static toDomain(certification: PrismaCertification): Certification {
+  static persistenceToDomain(
+    certification: PrismaCertification
+  ): Certification {
     return Certification.create(
       {
         certification: certification.certification,
@@ -12,5 +15,29 @@ export default class CertificationMapper {
       },
       new UniqueEntityID(certification.id)
     );
+  }
+
+  static dtoToDomain(certification: CertificationDTO, id?: string) {
+    return Certification.create(
+      {
+        certification: certification.certification,
+        institution: certification.institution,
+        year: certification.year,
+      },
+      id ? new UniqueEntityID(id) : new UniqueEntityID()
+    );
+  }
+
+  static toPersistence(
+    certification: Certification,
+    freelancerId: string
+  ): PrismaCertification {
+    return {
+      id: certification.certificationId.toString(),
+      certification: certification.certification,
+      institution: certification.institution,
+      year: new Date(certification.year, 0, 1),
+      freelancerId: freelancerId,
+    };
   }
 }

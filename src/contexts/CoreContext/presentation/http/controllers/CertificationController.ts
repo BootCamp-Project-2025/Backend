a036d8cfd56900010c22ffcdf3868c83dbac2994
Ariel Monroy <ariel.monroy@jala.university>
2025-06-13 00:10:14 -0400

    feat: implement CRUD operations for certifications in CertificationController and update routes

--- src/contexts/CoreContext/presentation/http/controllers/CertificationController.ts
+++ src/contexts/CoreContext/presentation/http/controllers/CertificationController.ts
@@ -8,7 +8,7 @@ export class CertificationController implements ICertificationController {
   constructor(
     @inject("ICertificationService")
     private readonly certificationService: ICertificationService
-  ) {}
+  ) { }
 
   async getByFreelancerId(req: Request, res: Response): Promise<void> {
     const freelancerId = req.params.id;
@@ -19,5 +19,41 @@ export class CertificationController implements ICertificationController {
     }
 
     res.status(200).json(certifications);
+  }
+
+  async getById(req: Request, res: Response): Promise<void> {
+    const certificationId = req.params.id;
+    const certification =
+      await this.certificationService.getById(certificationId);
+    if (!certification) {
+      throw new Error("Certification not found");
+    }
+
+    res.status(200).json(certification);
+  }
+
+  async create(req: Request, res: Response): Promise<void> {
+    const freelancerId = req.params.id;
+    const certification = req.body;
+    await this.certificationService.create(certification, freelancerId);
+    res.status(201).send();
+  }
+
+  async update(req: Request, res: Response): Promise<void> {
+    const freelancerId = req.params.id;
+    const certificationId = req.params.certificationId;
+    const certification = req.body;
+    await this.certificationService.update(
+      certificationId,
+      certification,
+      freelancerId
+    );
+    res.status(204).send();
+  }
+
+  async delete(req: Request, res: Response): Promise<void> {
+    const certificationId = req.params.id;
+    await this.certificationService.delete(certificationId);
+    res.status(204).send();
   }
 }

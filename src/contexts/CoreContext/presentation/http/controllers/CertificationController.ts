55ce22c3c363f3c2cd15facc48d32f1c277b6745
Ariel Monroy <ariel.monroy@jala.university>
2025-06-13 21:31:25 -0400

    fix: format with "format" command

--- src/contexts/CoreContext/presentation/http/controllers/CertificationController.ts
+++ src/contexts/CoreContext/presentation/http/controllers/CertificationController.ts
@@ -11,7 +11,7 @@ export class CertificationController implements ICertificationController {
   constructor(
     @inject("ICertificationService")
     private readonly certificationService: ICertificationService
-  ) { }
+  ) {}
 
   async getByFreelancerId(req: Request, res: Response): Promise<void> {
     const freelancerId = req.params.id;

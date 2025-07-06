1d3fc4f9e7880de90cc7234355a1daa79ab2e5c6
MauricioLsalles <mauricio.leano@jala.university>
2025-07-06 17:30:13 -0400

    feat: creted interfacecs for repository and service

---
+++ src/contexts/LearningContext/domain/interfaces/ILessonRepository.ts
@@ -0,0 +1,7 @@
+import { Lesson } from "../entities/Lesson";
+
+export default interface ILessonRepository {
+  create(module: Lesson, courseId: string): Promise<Lesson>;
+  delete(moduleId: string): Promise<void>;
+  update(module: Lesson): Promise<Lesson>;
+}

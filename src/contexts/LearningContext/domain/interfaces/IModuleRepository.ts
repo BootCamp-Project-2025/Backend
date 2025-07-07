7dcf07fd48831ec7f86cc1a6b9b3b17663b25611
MauricioLsalles <mauricio.leano@jala.university>
2025-07-06 22:59:50 -0400

    feat: aded findByid method

--- src/contexts/LearningContext/domain/interfaces/IModuleRepository.ts
+++ src/contexts/LearningContext/domain/interfaces/IModuleRepository.ts
@@ -2,6 +2,7 @@ import { Module } from "../entities/Module";
 
 export default interface IModuleRepository {
   findByCourseId(courseId: string): Promise<Module[]>;
+  findById(moduleId: string): Promise<Module | null>;
   create(module: Module, courseId: string): Promise<Module>;
   delete(moduleId: string): Promise<void>;
   update(module: Module): Promise<Module>;

0613129de81a77c82f897cf017014b9322bf4218
Ariel Monroy <ariel.monroy@jala.university>
2025-07-10 09:26:48 -0400

    fix: add dotenv configuration and log environment variables

--- src/contexts/CoreContext/infrastructure/keycloak/keycloakService.ts
+++ src/contexts/CoreContext/infrastructure/keycloak/keycloakService.ts
@@ -1,10 +1,16 @@
 import { IExternarlAuthService } from "../../domain/interfaces/services/IExternalAuthService";
 import { injectable } from "tsyringe";
 import { keycloakAxios } from "../../../Shared/infrastructure/axiosInstance";
+import dotenv from "dotenv";
+dotenv.config();
 
+console.log(`${process.env.KEYCLOAK_URL}`);
+console.log(`${process.env.KEYCLOAK_REALM}`);
+console.log(`${process.env.KEYCLOAK_CLIENT_ID}`);
+console.log(`${process.env.KEYCLOAK_CLIENT_SECRET}`);
 @injectable()
 export class KeycloakService implements IExternarlAuthService {
-  constructor() {}
+  constructor() { }
 
   keycloakUrl = process.env.KEYCLOAK_URL || "http://localhost:8080/auth";
   keycloakRealm = process.env.KEYCLOAK_REALM || "LTCrowd";
@@ -15,6 +21,7 @@ export class KeycloakService implements IExternarlAuthService {
     process.env.KEYCLOAK_BACKEND_CLIENT_ID || "backend-client";
 
   public async getAdminToken(): Promise<string> {
+
     const params = new URLSearchParams();
     params.append("grant_type", "client_credentials");
     params.append("client_id", this.keycloakClientId);

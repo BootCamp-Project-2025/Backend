bb2d5b47cb5d838da6e3dbe25fac13d96d42a7a5
cclaurevju <Carlos.Claure@jala.university>
2025-07-09 23:01:53 -0400

    fix: import env vars befor init app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import app from "./app";
 import dotenv from "dotenv";
+import app from "./app";
 
 dotenv.config();
 
+const API_HOST = process.env.API_HOST || "localhost";
 const API_PORT = process.env.API_PORT || 3000;
 
 app.listen(API_PORT, () => {
-  console.log(`Server running on http://localhost:${API_PORT}`);
+  console.log(`Server running on http://${API_HOST}:${API_PORT}`);
 });

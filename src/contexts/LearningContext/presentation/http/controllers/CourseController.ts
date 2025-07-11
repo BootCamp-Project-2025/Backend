6551daa453f2c1065bdd406ffe49bbc364f0226c
MauricioLsalles <mauricio.leano@jala.university>
2025-07-11 17:01:42 -0400

    fix lint

--- src/contexts/LearningContext/presentation/http/controllers/CourseController.ts
+++ src/contexts/LearningContext/presentation/http/controllers/CourseController.ts
@@ -15,6 +15,7 @@ export class CourseController implements ICourseController {
   constructor(
     @inject("ICourseService") private readonly courseService: ICourseService
   ) {}
+  /* eslint-disable */
   getCompleteCourse(req: Request, res: Response): Promise<void> {
     throw new Error("Method not implemented.");
   }

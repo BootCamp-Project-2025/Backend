fb8735b72398a6c322d4cced5c24b71ab9b761a5
Ariel Monroy <ariel.monroy@jala.university>
2025-07-13 22:09:04 -0400

    feat: remove unused methods from IEnrollmentService interface

--- src/contexts/CoreContext/domain/interfaces/services/IEnrollmentService.ts
+++ src/contexts/CoreContext/domain/interfaces/services/IEnrollmentService.ts
@@ -3,7 +3,4 @@ import { Enrollment } from "@/generated/prisma";
 export interface IEnrollmentService {
   create(enrollment: Enrollment): Promise<void>;
   cancel(enrollmentId: string): Promise<void>;
-  getById(enrollmentId: string): Promise<Enrollment | null>;
-  getByUserId(userId: string): Promise<Enrollment[]>;
-  getByCourseId(courseId: string): Promise<Enrollment[]>;
 }

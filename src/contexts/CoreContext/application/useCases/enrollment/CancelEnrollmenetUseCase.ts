5f434ef39ddeb6b279be745f5fcd634774ea5827
Ariel Monroy <ariel.monroy@jala.university>
2025-07-13 22:09:07 -0400

    feat: implement CreateEnrollmentUseCase for enrollment creation logic

---
+++ src/contexts/CoreContext/application/useCases/enrollment/CancelEnrollmenetUseCase.ts
@@ -0,0 +1,23 @@
+import { IEnrollmentRepository } from "@/contexts/CoreContext/domain/interfaces/repositories/IEnrollmentRepository";
+import IUseCase from "@/contexts/LearningContext/domain/interfaces/IUseCase";
+import { ApiError } from "@/contexts/Shared/infrastructure/errors/ApiError";
+import { StatusCodes } from "http-status-codes";
+import { inject } from "tsyringe";
+
+export class CancelEnrollmentUseCase
+  implements IUseCase<{ enrollmentId: string }, void> {
+  constructor(
+    @inject("IEnrollmentService")
+    private enrollmentRepository: IEnrollmentRepository
+  ) { }
+
+  async execute(params: { enrollmentId: string }): Promise<void> {
+    const enrollment = await this.enrollmentRepository.findById(
+      params.enrollmentId
+    );
+    if (!enrollment)
+      throw new ApiError(StatusCodes.NOT_FOUND, "Enrollment not found");
+    enrollment.cancel();
+    await this.enrollmentRepository.cancelEnrollment(enrollment);
+  }
+}
---
+++ src/contexts/CoreContext/application/useCases/enrollment/CreateEnrollmentUseCase.ts
@@ -0,0 +1,37 @@
+import { Enrollment } from "@/contexts/CoreContext/domain/aggregates/Enrollment";
+import { IEnrollmentRepository } from "@/contexts/CoreContext/domain/interfaces/repositories/IEnrollmentRepository";
+import { IUserRepository } from "@/contexts/CoreContext/domain/interfaces/repositories/IUserRepository";
+import { ICourseRepository } from "@/contexts/LearningContext/domain/interfaces/ICourseRepository";
+import IUseCase from "@/contexts/LearningContext/domain/interfaces/IUseCase";
+import { ApiError } from "@/contexts/Shared/infrastructure/errors/ApiError";
+import { StatusCodes } from "http-status-codes";
+import { inject } from "tsyringe";
+
+export class CreateEnrollmentUseCase
+  implements IUseCase<Enrollment, Enrollment> {
+  constructor(
+    @inject("IEnrollmentRepository")
+    private enrollmentRepository: IEnrollmentRepository,
+    @inject("ICourseRepository")
+    private courseRepository: ICourseRepository,
+    @inject("IUserRepository")
+    private userRepository: IUserRepository
+  ) { }
+
+  async execute(enrollment: Enrollment): Promise<Enrollment> {
+    const user = await this.userRepository.getById(
+      enrollment.userId.toString()
+    );
+    if (!user) {
+      throw new ApiError(StatusCodes.NOT_FOUND, "User not found");
+    }
+    const course = await this.courseRepository.findById(
+      enrollment.userId.toString()
+    );
+    if (!course) {
+      throw new ApiError(StatusCodes.NOT_FOUND, "Course not found");
+    }
+
+    return await this.enrollmentRepository.create(enrollment);
+  }
+}

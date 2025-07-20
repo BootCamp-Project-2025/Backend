2a14210dec93ed532f7d3d7e5354fc72ea99f393
MauricioLsalles <mauricio.leano@jala.university>
2025-07-20 18:01:39 -0400

    feat: create request use case created with tests

---
+++ src/contexts/CoreContext/application/useCases/requests/CreateRequestUseCase.ts
@@ -0,0 +1,16 @@
+import { Request } from "@/contexts/CoreContext/domain/aggregates/Request";
+import IRequestRepository from "@/contexts/CoreContext/domain/interfaces/repositories/IRequestRepository";
+import IUseCase from "@/contexts/LearningContext/domain/interfaces/IUseCase";
+import { inject } from "tsyringe";
+
+export default class CreateRequestUseCase
+  implements IUseCase<Request, Request>
+{
+  constructor(
+    @inject("IRequestRepository")
+    private readonly repository: IRequestRepository
+  ) {}
+  async execute(request: Request): Promise<Request> {
+    return await this.repository.create(request);
+  }
+}

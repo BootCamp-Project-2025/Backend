56dec02284c6d4af0cc64500d6dde51f11acdd8b
AndreCarpio <Andre.Carpio@jala.university>
2025-07-23 14:41:48 -0400

    feat: Creating use case to get a user's enrollments.

---
+++ src/contexts/CoreContext/application/useCases/GetUserEnrollmentsUseCase.ts
@@ -0,0 +1,38 @@
+import { inject, injectable } from "tsyringe";
+import { IEnrollmentRepository } from "../../domain/interfaces/repositories/IEnrollmentRepository";
+import { Enrollment } from "../../domain/aggregates/Enrollment";
+import { ApiError } from "@/contexts/Shared/infrastructure/errors/ApiError";
+import { StatusCodes } from "http-status-codes";
+import { IUserRepository } from "../../domain/interfaces/repositories/IUserRepository";
+
+@injectable()
+export class GetUserEnrollmentsUseCase {
+  constructor(
+    @inject("IEnrollmentRepository")
+    private readonly enrollmentRepo: IEnrollmentRepository,
+    @inject("IUserRepository")
+    private readonly userRepository: IUserRepository
+  ) {}
+
+  async execute(userId: string): Promise<Enrollment[]> {
+    try {
+      if (!userId) {
+        throw new ApiError(StatusCodes.BAD_REQUEST, "User ID is required.");
+      }
+
+      const userExists = await this.userRepository.getById(userId);
+      if (!userExists) {
+        throw new ApiError(StatusCodes.NOT_FOUND, "User not found.");
+      }
+
+      const enrollments = await this.enrollmentRepo.getByUserId(userId);
+      return enrollments;
+    } catch (error) {
+      if (error instanceof ApiError) throw error;
+      throw new ApiError(
+        StatusCodes.INTERNAL_SERVER_ERROR,
+        "Error fetching enrollments"
+      );
+    }
+  }
+}

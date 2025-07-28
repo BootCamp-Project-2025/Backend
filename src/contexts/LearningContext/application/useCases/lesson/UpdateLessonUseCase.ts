ec1752c8ae6fab7dbc120b56bb090c2f878cbfef
Ariel Monroy <ariel.monroy@jala.university>
2025-07-28 00:31:20 -0400

    feat: enhance UpdateLessonUseCase to update resource URLs using ICdnService

--- src/contexts/LearningContext/application/useCases/lesson/UpdateLessonUseCase.ts
+++ src/contexts/LearningContext/application/useCases/lesson/UpdateLessonUseCase.ts
@@ -1,6 +1,8 @@
+import { ICdnService } from "@/contexts/CoreContext/domain/interfaces/services/ICdnService";
 import { Lesson } from "@/contexts/LearningContext/domain/entities/Lesson";
 import ILessonRepository from "@/contexts/LearningContext/domain/interfaces/ILessonRepository";
 import IUseCase from "@/contexts/LearningContext/domain/interfaces/IUseCase";
+import { LessonResource } from "@/contexts/LearningContext/domain/valueObjects/LessonResource";
 import { ApiError } from "@/contexts/Shared/infrastructure/errors/ApiError";
 import { StatusCodes } from "http-status-codes";
 import { inject, injectable } from "tsyringe";
@@ -9,18 +11,48 @@ import { inject, injectable } from "tsyringe";
 export default class UpdateLessonUseCase implements IUseCase<Lesson, Lesson> {
   constructor(
     @inject("ILessonRepository")
-    private readonly lessonRepository: ILessonRepository
+    private readonly lessonRepository: ILessonRepository,
+    @inject("ICdnService")
+    private readonly cdnService: ICdnService
   ) {}
   async execute(lesson: Lesson): Promise<Lesson> {
     try {
       const existingLesson = await this.lessonRepository.findById(
         lesson.id.toString()
       );
+
       if (!existingLesson) {
         throw new ApiError(StatusCodes.NOT_FOUND, "Lesson not found");
       }
-      if (existingLesson.compareTo(lesson))
+      if (existingLesson.compareTo(lesson)) {
         throw new ApiError(StatusCodes.BAD_REQUEST, "Lesson not changed");
+      }
+
+      let lessonUrls: LessonResource[] = [];
+
+      if (lesson.props.resources) {
+        const updatedResources = await Promise.all(
+          lesson.props.resources.map(async (resource) => {
+            const url = await this.cdnService.updateFilePreset(resource.url);
+            if (!url) {
+              throw new ApiError(
+                StatusCodes.INTERNAL_SERVER_ERROR,
+                "Failed to update resource URL"
+              );
+            }
+            const newResource = LessonResource.create({
+              url,
+              name: resource.name,
+            });
+            await this.cdnService.deleteFile(resource.url);
+            return newResource;
+          })
+        );
+
+        lessonUrls = updatedResources;
+      }
+
+      lesson.updateResourcesUrl(lessonUrls);
       return await this.lessonRepository.update(lesson);
     } catch (error) {
       if (error instanceof ApiError) throw error;

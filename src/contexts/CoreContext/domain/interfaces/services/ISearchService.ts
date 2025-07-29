a64a6aedb64df5375386e4faa0f8ba734c17f447
Ariel Monroy <ariel.monroy@jala.university>
2025-07-29 16:50:31 -0400

    feat: update ISearchService to return SearchResponse and include indexResource method

--- src/contexts/CoreContext/domain/interfaces/services/ISearchService.ts
+++ src/contexts/CoreContext/domain/interfaces/services/ISearchService.ts
@@ -1,6 +1,12 @@
-import { PageDto } from "../dtos/search/PageDto";
+import { SearchResponse } from "@elastic/elasticsearch/lib/api/types";
 import { SearchQueryDto } from "../dtos/search/SearchQueryDto";
+import { CourseDTO } from "@/contexts/LearningContext/domain/dtos/CourseDTO";
+import { RequestDto } from "../dtos/RequestDto";
 
 export interface ISearchService {
-  search(params: SearchQueryDto): Promise<PageDto>;
+  search(params: SearchQueryDto): Promise<SearchResponse>;
+  indexResource(
+    resource: string,
+    resourceDto: CourseDTO | RequestDto
+  ): Promise<void>;
 }

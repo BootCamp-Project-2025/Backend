3fb6a3b28291c66d6b4b37fe9443b49a29bab3fc
Ariel Monroy <ariel.monroy@jala.university>
2025-07-29 16:50:38 -0400

    feat: add indexResource method to ElasticSearchService for indexing resources

--- src/contexts/CoreContext/application/services/ElasticSearchService.ts
+++ src/contexts/CoreContext/application/services/ElasticSearchService.ts
@@ -3,6 +3,8 @@ import { SearchQueryDto } from "../../domain/interfaces/dtos/search/SearchQueryD
 import { ISearchService } from "../../domain/interfaces/services/ISearchService";
 import { Client } from "@elastic/elasticsearch";
 import { SearchResponse } from "@elastic/elasticsearch/lib/api/types";
+import { CourseDTO } from "@/contexts/LearningContext/domain/dtos/CourseDTO";
+import { RequestDto } from "../../domain/interfaces/dtos/RequestDto";
 
 @injectable()
 export class ElasticSearchService implements ISearchService {
@@ -20,5 +22,15 @@ export class ElasticSearchService implements ISearchService {
 
   async search(params: SearchQueryDto): Promise<SearchResponse> {
     return await this.esClient.search(params);
+  }
+
+  async indexResource(
+    resource: string,
+    resourceDto: CourseDTO | RequestDto
+  ): Promise<void> {
+    await this.esClient.index({
+      index: resource,
+      document: resourceDto,
+    });
   }
 }

15d7d566581fb27e5c03db8f6ed4e4d275506a97
Ariel Monroy <ariel.monroy@jala.university>
2025-07-30 16:55:29 -0400

    feat: add RequestIndex interface and domainToIndex method in RequestMapper for request indexing

---
+++ src/contexts/CoreContext/domain/interfaces/dtos/index/RequestIndex.ts
@@ -0,0 +1,10 @@
+export interface RequestIndex {
+  id?: string;
+  title?: string;
+  description?: string;
+  language?: string;
+  category?: string;
+  subCategory?: string;
+  userId?: string;
+  createdAt?: Date;
+}

4d5f15ffb5618aab3582ee207e88a986d08d747b
Ariel Monroy <ariel.monroy@jala.university>
2025-07-30 16:56:33 -0400

    feat: register IndexResourceEvent handler in global event dispatcher

--- src/eventRegister.ts
+++ src/eventRegister.ts
@@ -1,10 +1,13 @@
 import { sendExampleMsg } from "./contexts/Shared/application/handlers/SendExampleMsg";
 import { EventDispatcher } from "./contexts/Shared/domain/events/EventDispatcher";
 import { ExampleCreatedEvent } from "./contexts/Shared/domain/events/ExampleCreatedEvent";
+import { IndexResourceEvent } from "./contexts/Shared/domain/events/IndexResourceEvent";
+import { indexResourceHandler } from "./contexts/Shared/application/handlers/IndexResourceHandler";
 
-const globalEventDispatcher = new EventDispatcher();
+export const globalEventDispatcher = new EventDispatcher();
 
 globalEventDispatcher.register(ExampleCreatedEvent.name, sendExampleMsg);
+globalEventDispatcher.register(IndexResourceEvent.name, indexResourceHandler);
 
 // Example usage of the event dispatcher
 // const event = new ExampleCreatedEvent({

888f355339c6bda2e6d115bd6c803edb3b66cd97
MauricioLsalles <mauricio.leano@jala.university>
2025-07-31 17:19:37 -0400

    feat: created dtos for p2p course and its entitties

---
+++ src/contexts/LearningContext/domain/dtos/P2PCourseDTO.ts
@@ -0,0 +1,16 @@
+import { FilePostDTO } from "./FilePostDTO";
+import { PostDTO } from "./PostDTO";
+import { SessionDTO } from "./SessionDTO";
+
+export type P2PCourseDTO = {
+  id: string;
+  studentId: string;
+  teacherId: string;
+  chatId: string;
+  name: string;
+  remainingSession: number;
+  status: "ACTIVE" | "CANCELED" | "COMPLETED";
+  posts: PostDTO[];
+  files: FilePostDTO[];
+  sessions: SessionDTO[];
+};

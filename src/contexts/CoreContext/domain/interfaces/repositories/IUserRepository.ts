fc63292db81cab524161af5cb61f8097004faac7
AndreCarpio <Andre.Carpio@jala.university>
2025-07-31 10:51:51 -0400

    feat: create method  in the repository to get  user's courses

--- src/contexts/CoreContext/domain/interfaces/repositories/IUserRepository.ts
+++ src/contexts/CoreContext/domain/interfaces/repositories/IUserRepository.ts
@@ -1,8 +1,10 @@
 import { IRepository } from "@/contexts/Shared/domain/repository/IRepository";
 import { User, UserProps } from "../../aggregates/User";
+import { Course } from "@/contexts/LearningContext/domain/aggregates/Course";
 
 export interface IUserRepository extends IRepository<User> {
   addFreelancerProfile(id: string): Promise<User>;
   getUserProfileById(id: string): Promise<User | null>;
   updateUserProfile(id: string, user: Partial<UserProps>): Promise<User | null>;
+  getCourses(userId: string): Promise<Course[]>;
 }

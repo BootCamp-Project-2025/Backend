24ebfa1d7aaea573b737cf91d4f892ee2710b482
AndreCarpio <Andre.Carpio@jala.university>
2025-07-31 10:53:38 -0400

    feat: update controller and service to get user's courses

--- src/contexts/CoreContext/domain/interfaces/services/IUserService.ts
+++ src/contexts/CoreContext/domain/interfaces/services/IUserService.ts
@@ -1,8 +1,10 @@
 import { IService } from "@/contexts/Shared/domain/service/IService";
 import { User, UserProps } from "../../aggregates/User";
+import { Course } from "@/contexts/LearningContext/domain/aggregates/Course";
 export interface IUserService extends IService<User> {
   delete(id: string): Promise<string | void>;
   addRole(role: string): Promise<void>;
   createFreelanceProfile(id: string): Promise<User>;
   updateUserProfile(id: string, user: Partial<UserProps>): Promise<User | null>;
+  getCourses(userId: string): Promise<Course[]>;
 }

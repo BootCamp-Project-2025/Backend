2eb6de5db29b891861a18dbe51aea9aa3ffa4193
MauricioLsalles <mauricio.leano@jala.university>
2025-08-01 10:55:16 -0400

    refactor: removed unused code

--- src/contexts/LearningContext/application/useCases/p2pCourse/AddFilePostUseCase.ts
+++ src/contexts/LearningContext/application/useCases/p2pCourse/AddFilePostUseCase.ts
@@ -1,37 +1,28 @@
+import { P2PCourse } from "@/contexts/LearningContext/domain/aggregates/P2PCourse";
 import { FilePost } from "@/contexts/LearningContext/domain/entities/FilePost";
 import IFilePostRepository from "@/contexts/LearningContext/domain/interfaces/IFilePostRepository";
-import IP2PCourseRepository from "@/contexts/LearningContext/domain/interfaces/IP2PCourseRepository";
 import IUseCase from "@/contexts/LearningContext/domain/interfaces/IUseCase";
-import { ApiError } from "@/contexts/Shared/infrastructure/errors/ApiError";
-import { StatusCodes } from "http-status-codes";
 import { inject, injectable } from "tsyringe";
 
 @injectable()
 export default class AddFilePostUseCase
-  implements IUseCase<{ p2pCourseId: string; filePost: FilePost }, FilePost>
+  implements IUseCase<{ p2pCourse: P2PCourse; filePost: FilePost }, FilePost>
 {
   constructor(
-    @inject("IP2PCourseRepository")
-    private readonly p2pCourseRepository: IP2PCourseRepository,
     @inject("IFilePostRepository")
     private readonly filePostRepository: IFilePostRepository
   ) {}
   async execute({
-    p2pCourseId,
+    p2pCourse,
     filePost,
   }: {
-    p2pCourseId: string;
+    p2pCourse: P2PCourse;
     filePost: FilePost;
   }): Promise<FilePost> {
-    const p2pCourse = await this.p2pCourseRepository.findById(p2pCourseId);
-    if (!p2pCourse) {
-      throw new ApiError(StatusCodes.NOT_FOUND, "The course doesn't exist");
-    }
-
     p2pCourse.addFilePost(filePost);
 
     const savedFilePost = await this.filePostRepository.create(
-      p2pCourseId,
+      p2pCourse.id.toString(),
       filePost
     );
 

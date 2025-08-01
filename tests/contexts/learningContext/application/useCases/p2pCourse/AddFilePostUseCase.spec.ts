5cb02ec7edf4026ba75a23b8ec4815ee839bc734
MauricioLsalles <mauricio.leano@jala.university>
2025-08-01 10:55:16 -0400

    refactor: removed unused code

--- tests/contexts/learningContext/application/useCases/p2pCourse/AddFilePostUseCase.spec.ts
+++ tests/contexts/learningContext/application/useCases/p2pCourse/AddFilePostUseCase.spec.ts
@@ -9,32 +9,14 @@ describe("AddFilePostUseCase component", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
-  const p2pCourseRepositoryMock = {
-    create: jest.fn(),
-    findById: jest.fn(),
-    findByUserIdAndCourseId: jest.fn(),
-  };
+
   const filePostRepositoryMock = {
     create: jest.fn(),
     delete: jest.fn(),
   };
-  const useCase = new AddFilePostUseCase(
-    p2pCourseRepositoryMock,
-    filePostRepositoryMock
-  );
+  const useCase = new AddFilePostUseCase(filePostRepositoryMock);
   it("Creates correctly", () => {
     expect(useCase).toBeInstanceOf(AddFilePostUseCase);
-  });
-
-  it("Throws error if there is no course", () => {
-    const filePost = FilePost.createFromPrimitive({
-      url: "https://www.youtube.com/",
-      creationDate: new Date(),
-    });
-    p2pCourseRepositoryMock.findById.mockResolvedValue(null);
-    expect(
-      useCase.execute({ p2pCourseId: "p2pCourseTestId", filePost })
-    ).rejects.toThrow(ApiError);
   });
 
   it("Executes correctly", () => {
@@ -53,10 +35,7 @@ describe("AddFilePostUseCase component", () => {
       files: [],
       sessions: [],
     });
-    p2pCourseRepositoryMock.findById.mockResolvedValue(p2pCourse);
     filePostRepositoryMock.create.mockResolvedValue(filePost);
-    expect(
-      useCase.execute({ p2pCourseId: "p2pCourseTestId", filePost })
-    ).resolves.toBe(filePost);
+    expect(useCase.execute({ p2pCourse, filePost })).resolves.toBe(filePost);
   });
 });

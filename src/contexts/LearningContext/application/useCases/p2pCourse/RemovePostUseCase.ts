c1758a3aeb869be7ced9792ec91ab80dfbe0c4cd
MauricioLsalles <mauricio.leano@jala.university>
2025-08-01 15:07:16 -0400

    feat: created RemovePost use case with tests

---
+++ src/contexts/LearningContext/application/useCases/p2pCourse/RemovePostUseCase.ts
@@ -0,0 +1,23 @@
+import { P2PCourse } from "@/contexts/LearningContext/domain/aggregates/P2PCourse";
+import IPostRepository from "@/contexts/LearningContext/domain/interfaces/IPostRepository";
+import IUseCase from "@/contexts/LearningContext/domain/interfaces/IUseCase";
+import { UniqueEntityID } from "@/contexts/Shared/domain/UniqueEntityID";
+import { inject } from "tsyringe";
+
+export default class RemovePostUseCase
+  implements IUseCase<{ p2pCourse: P2PCourse; postId: string }, void>
+{
+  constructor(
+    @inject("IPostRepository") private readonly postRepository: IPostRepository
+  ) {}
+  async execute({
+    p2pCourse,
+    postId,
+  }: {
+    p2pCourse: P2PCourse;
+    postId: string;
+  }): Promise<void> {
+    p2pCourse.deletePost(new UniqueEntityID(postId));
+    await this.postRepository.delete(postId);
+  }
+}

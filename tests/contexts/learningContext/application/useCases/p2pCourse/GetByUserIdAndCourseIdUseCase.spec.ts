a7d6f8faa20d334255b0c76d1ec1d96531868bc7
MauricioLsalles <mauricio.leano@jala.university>
2025-08-01 09:31:33 -0400

    feat: created getByuserIdAndCourseId use case with tests

---
+++ tests/contexts/learningContext/application/useCases/p2pCourse/GetByUserIdAndCourseIdUseCase.spec.ts
@@ -0,0 +1,45 @@
+import "reflect-metadata";
+import GetByUserIdAndCourseIdUseCase from "@/contexts/LearningContext/application/useCases/p2pCourse/GetByUserIdAndCourseIdUseCase";
+import { ApiError } from "@/contexts/Shared/infrastructure/errors/ApiError";
+import { P2PCourse } from "@/contexts/LearningContext/domain/aggregates/P2PCourse";
+
+describe("GetByUserIdAndCourseIdUseCase component", () => {
+  const p2pCourseRepositoryMock = {
+    create: jest.fn(),
+    findByUserId: jest.fn(),
+    findByUserIdAndCourseId: jest.fn(),
+  };
+  it("Creates correctly", () => {
+    const useCase = new GetByUserIdAndCourseIdUseCase(p2pCourseRepositoryMock);
+    expect(useCase).toBeInstanceOf(GetByUserIdAndCourseIdUseCase);
+  });
+
+  it("Throws error if there is no course found", async () => {
+    const useCase = new GetByUserIdAndCourseIdUseCase(p2pCourseRepositoryMock);
+    p2pCourseRepositoryMock.findByUserIdAndCourseId.mockResolvedValue(null);
+    expect(
+      useCase.execute({ p2pCourseId: "p2pCourseTestId", userId: "userTestId" })
+    ).rejects.toThrow(ApiError);
+  });
+
+  it("Return a course if its found", async () => {
+    const useCase = new GetByUserIdAndCourseIdUseCase(p2pCourseRepositoryMock);
+    const p2pCourse = P2PCourse.createFromPrimitive({
+      studentId: "studentId",
+      teacherId: "teacherId",
+      chatId: "chatId",
+      name: "Test name",
+      remainingSession: 5,
+      status: "ACTIVE",
+      posts: [],
+      files: [],
+      sessions: [],
+    });
+    p2pCourseRepositoryMock.findByUserIdAndCourseId.mockResolvedValue(
+      p2pCourse
+    );
+    expect(
+      useCase.execute({ p2pCourseId: "p2pCourseTestId", userId: "userTestId" })
+    ).resolves.toBe(p2pCourse);
+  });
+});

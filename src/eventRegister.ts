39842f3e64d836c21c6847bb7cf108b3c0beb318
Ariel Monroy <ariel.monroy@jala.university>
2025-08-03 22:57:02 -0400

    feat: implement delete resource handling and improve logging in DeleteRequestUseCase

--- src/eventRegister.ts
+++ src/eventRegister.ts
@@ -3,11 +3,14 @@ import { EventDispatcher } from "./contexts/Shared/domain/events/EventDispatcher
 import { ExampleCreatedEvent } from "./contexts/Shared/domain/events/ExampleCreatedEvent";
 import { IndexResourceEvent } from "./contexts/Shared/domain/events/IndexResourceEvent";
 import { indexResourceHandler } from "./contexts/Shared/application/handlers/IndexResourceHandler";
+import { DeleteResourceEvent } from "./contexts/Shared/domain/events/DeleteResourceEvent";
+import { deleteResourceHandler } from "./contexts/Shared/application/handlers/DeleteResourceHandler";
 
 export const globalEventDispatcher = new EventDispatcher();
 
 globalEventDispatcher.register(ExampleCreatedEvent.name, sendExampleMsg);
 globalEventDispatcher.register(IndexResourceEvent.name, indexResourceHandler);
+globalEventDispatcher.register(DeleteResourceEvent.name, deleteResourceHandler);
 
 // Example usage of the event dispatcher
 // const event = new ExampleCreatedEvent({
